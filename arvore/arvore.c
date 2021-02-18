31ee5d39dd7435ecb8523293f137a89fe11f9fae
Vitor Ghiraldelli <vitorghis@gmail.com>
2021-02-18 09:16:53 -0300

    arvore

---
+++ arvore/arvore.c
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "arvore.h"
+
+p_no criar_arvore(int x, p_no esq, p_no dir) {
+    // TODO
+}
---
+++ arvore/arvore.h
@@ -0,0 +1,14 @@
+typedef struct No {
+    int dado;
+    struct No *esq, *dir; // *pai
+} No;
+
+typedef No * p_no;
+
+p_no criar_arvore(int x, p_no esq, p_no dir);
+
+p_no procurar_no(p_no raiz, int x);
+
+int numero_nos(p_no raiz);
+
+int altura(p_no raiz);
