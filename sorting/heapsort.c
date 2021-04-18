9765952dee122cc9e93d9c0bce68da73598523c3
Vitor Ghiraldelli <vitorghis@gmail.com>
2021-04-18 09:49:38 -0300

    refactoring heapsort

--- sorting/heapsort.c
+++ sorting/heapsort.c
@@ -17,13 +17,13 @@ void fpsort(Item *v, int n) {
 }
 
 // Melhorando...
-void troca_int(int *a, int *b) {
+void swap(int *a, int *b) {
     int t = *a;
     *a = *b;
     *b = t;
 }
 
-void desce_no_heap_2(int *heap, int n, int k) {
+void heapify(int *heap, int n, int k) {
     int maior_filho;
     if (F_ESQ(k) < n) {
         maior_filho = F_ESQ(k);
@@ -31,33 +31,35 @@ void desce_no_heap_2(int *heap, int n, int k) {
             maior_filho = F_DIR(k);
 
         if (heap[k] < heap[maior_filho]) {
-            troca_int(&heap[k], &heap[maior_filho]);
-            desce_no_heap_2(heap, n, maior_filho);
+            swap(&heap[k], &heap[maior_filho]);
+            heapify(heap, n, maior_filho);
         }
     }
 }
 
 void heapsort(int *v, int n) {
     for (int k = n/2; k >= 0; k--) /* transforma em heap */
-        desce_no_heap_2(v, n, k);
+        heapify(v, n, k);
     while (n > 1) { /* extrai o máximo */
-        troca_int(&v[0], &v[n - 1]);
+        swap(&v[0], &v[n - 1]);
         n--;
-        desce_no_heap_2(v, n, 0);
+        heapify(v, n, 0);
     }
 }
 
 int main() {
-    int v[100000];
-    int n = 100000;
-    for (int i = 0; i < n; i++)
-        v[i] = rand();
+    //int v[100000];
+    //int n = 100000;
+    //for (int i = 0; i < n; i++)
+    //    v[i] = rand();
 
+    int v[] = {7,9,11,13,1,5,6};
+    int n = 7;
     heapsort(v, n);
 
-    //for (int i = 0; i < n; i++)
-    //    printf("%d ", v[i]);
-    //printf("\n");
+    for (int i = 0; i < n; i++)
+        printf("%d ", v[i]);
+    printf("\n");
 
     return 0;
 }
