723aa85ea3f5b5ba5e5347348352c02ef3112ef3
Vitor Ghiraldelli <vitorghis@gmail.com>
2021-09-28 20:52:33 -0300

    adding dfs usage

--- grafos/traversal/depth-first-search.c
+++ grafos/traversal/depth-first-search.c
@@ -16,13 +16,40 @@ int dfs(p_grafo g, int v, int t, int *visitados) {
 }
 
 int ha_caminho(p_grafo g, int s, int t) {
-    int *visitados = malloc(g-> n * sizeof(int));
+    int *visitados = malloc(g->n * sizeof(int));
     for(int i = 0; i < g->n; i++)
         visitados[i] = 0;
 
     int ha_caminho = dfs(g, s, t, visitados);
     free(visitados);
     return ha_caminho;
+}
+
+void dfs_caminhos(p_grafo g, int *pai, int p, int v) {
+    pai[v] = p;
+    for (p_no t = g->adjacencia[v]; t != NULL; t = t->prox)
+        if (pai[t->v] == -1)
+            dfs_caminhos(g, pai, v, t->v);
+}
+
+int * encontra_caminhos(p_grafo g, int s) {
+    int *pai = malloc(g->n * sizeof(int));
+    for (int i = 0; i < g->n; i++)
+        pai[i] = -1;
+    dfs_caminhos(g, pai, s, s);
+    return pai;
+}
+
+void imprime_caminho(int *pai, int v) {
+    if (pai[v] != v)
+        imprime_caminho(pai, pai[v]);
+    printf("%d ", v);
+}
+
+void imprime_caminho_reverso(int *pai, int v) {
+    printf("%d ", v);
+    if (pai[v] != v)
+        imprime_caminho_reverso(pai, pai[v]);
 }
 
 int main() {
@@ -33,6 +60,13 @@ int main() {
     int encontrou = ha_caminho(g, s, t);
     char *encontrou_string = encontrou ? "SIM" : "NAO";
     printf("ha caminho entre s:%d e t:%d? %s\n", s, t, encontrou_string);
+
+    int *caminhos = encontra_caminhos(g, 0);
+    imprime_caminho(caminhos, 7);
+    printf("\n");
+    imprime_caminho_reverso(caminhos, 7);
+    printf("\n");
+    free(caminhos);
 
     destroi_grafo(g);
     return 0;
