343fe34ac9f621c1c6e4fefd0dc8b3478fa330b1
Vitor Ghiraldelli <vitorghis@gmail.com>
2021-10-06 09:12:46 -0300

    adding BFS and iterative DFS

--- grafos/traversal/dfs-iterative.c
+++ grafos/traversal/dfs-iterative.c
@@ -1,28 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "../representation/grafo-lista.h"
-#include "../../fila/fila.h"
+#include "../../pilha/pilha.h"
 
-void bfs_caminhos(p_grafo g, int *pai, int s) {
+void dfs_caminhos(p_grafo g, int *pai, int s) {
     int *visitado = calloc(g->n, sizeof(int));
-    p_fila fila = criar_fila();
+    p_pilha pilha = criar_pilha();
 
-    enfileirar(fila, s);
+    empilhar(pilha, s);
     pai[s] = s;
-    visitado[s] = 1;
 
-    while(!eh_vazia(fila)) {
-        int v = desenfileirar(fila);
+    while(!eh_vazia(pilha)) {
+        int v = desempilhar(pilha);
+        visitado[v] = 1;
         for (p_no w = g->adjacencia[v]; w != NULL; w = w->prox) {
             if (!visitado[w->dado]) {
-                visitado[w->dado] = 1;
                 pai[w->dado] = v;
-                enfileirar(fila, w->dado);
+                empilhar(pilha, w->dado);
             }
         }
     }
 
-    destruir_fila(fila);
+    destruir_pilha(pilha);
     free(visitado);
     return pai;
 }
@@ -31,6 +30,6 @@ int * encontra_caminhos(p_grafo g, int s) {
     int *pai = malloc(g->n * sizeof(int));
     for (int i = 0; i < g->n; i++)
         pai[i] = -1;
-    bfs_caminhos(g, pai, s);
+    dfs_caminhos(g, pai, s);
     return pai;
 }
